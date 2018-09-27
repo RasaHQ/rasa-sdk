91423353e9d551aba7bab230f6e882bc8af97ba7
Vova Vv <mr.voov@gmail.com>
2018-09-27 11:51:21 +0200

    change order: first deactivate form than set requested slot to None RasaHQ/roadmap#263

--- rasa_core_sdk/forms.py
+++ rasa_core_sdk/forms.py
@@ -84,7 +84,7 @@ class FormAction(Action):
         # there is nothing more to request, so we can submit
         events_from_submit = self.submit(dispatcher, temp_tracker, domain) or []
 
-        return events + events_from_submit + [SlotSet(REQUESTED_SLOT, None), Form(None)]
+        return events + events_from_submit + [Form(None), SlotSet(REQUESTED_SLOT, None)]
 
     def submit(self, dispatcher, tracker, domain):
         raise NotImplementedError(
