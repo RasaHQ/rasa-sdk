13d7ba8d78256d6f5241b88282a963f1249b1e31
Kniesel Guido <guido.kniesel@ihomelab.ch>
2019-02-06 10:28:52 +0100

    added ReminderCancelled event

--- rasa_core_sdk/events.py
+++ rasa_core_sdk/events.py
@@ -81,6 +81,15 @@ def ReminderScheduled(action_name, trigger_date_time, name=None,
 
 
 # noinspection PyPep8Naming
+def ReminderCancelled(name, timestamp=None):
+    return {
+        "event": "cancel",
+        "timestamp": timestamp,
+        "name": name
+    }
+
+
+# noinspection PyPep8Naming
 def ActionReverted(timestamp=None):
     return {
         "event": "undo",
