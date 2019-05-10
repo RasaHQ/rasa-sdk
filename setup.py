e06c4c723a2d89ed3147e0d172c8f4f7f0ce7799
Tom Bocklisch <tom@rasa.com>
2019-05-10 17:17:54 +0200

    fixed version identifiers

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ tests_requires = ["pytest~=3.0", "pytest-pep8~=1.0", "pytest-cov~=2.0"]
 install_requires = [
     "future~=0.17",
     "ConfigArgParse~=0.14",
-    "coloredlogs~=10",
+    "coloredlogs~=10.0",
     "flask~=1.0",
-    "flask_cors~=3",
+    "flask_cors~=3.0",
     "gevent>=1.4",
     "six>=1.10",
 ]
