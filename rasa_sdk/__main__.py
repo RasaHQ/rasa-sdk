9f6be7e1c76486e7e00d679c12586bced1bc229f
Tom Bocklisch <tom@rasa.com>
2019-05-13 12:51:08 +0200

    made main args accessible

--- rasa_sdk/__main__.py
+++ rasa_sdk/__main__.py
@@ -9,17 +9,21 @@ from rasa_sdk import utils
 from rasa_sdk.endpoint import create_argument_parser, run
 
 
+def main_from_args(args):
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("matplotlib").setLevel(logging.WARN)
+
+    utils.configure_colored_logging(args.loglevel)
+
+    run(args.actions, args.port, args.cors)
+
+
 def main():
     # Running as standalone python application
     arg_parser = create_argument_parser()
     cmdline_args = arg_parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger("matplotlib").setLevel(logging.WARN)
-
-    utils.configure_colored_logging(cmdline_args.loglevel)
-
-    run(cmdline_args.actions, cmdline_args.port, cmdline_args.cors)
+    main_from_args(cmdline_args)
 
 
 if __name__ == "__main__":
