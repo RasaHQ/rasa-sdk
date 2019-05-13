3c6d2a60961c49fcfc75e31d2739ad99c4ae471d
Tom Bocklisch <tom@rasa.com>
2019-05-13 12:26:24 +0200

    added backwards compatibility package

---
+++ rasa_core_sdk/__init__.py
@@ -0,0 +1,13 @@
+import rasa_sdk
+import sys
+import warnings
+
+# this makes sure old code can still import from `rasa_core`
+# although the package has been moved to `rasa.core`
+sys.modules["rasa_core_sdk"] = rasa_sdk
+
+warnings.warn(
+    "The 'rasa_core_sdk' package has been renamed. You should change "
+    "your imports to use 'rasa_sdk' instead.",
+    UserWarning,
+)
