72361faa49632bcdef0c248526c30f1dda42e5a8
Ella Rohm-Ensing <erohmensing@gmail.com>
2019-05-23 15:33:46 +0200

    re-add requests

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ tests_requires = ["pytest~=3.0", "pytest-pep8~=1.0", "pytest-cov~=2.0"]
 
 install_requires = [
     "future~=0.17",
+    "requests~=2.22",
     "ConfigArgParse~=0.14",
     "coloredlogs~=10.0",
     "flask~=1.0",
