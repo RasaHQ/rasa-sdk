65195e3da5720877394c5655a1ddec24df681460
Ella <erohmensing@gmail.com>
2019-11-27 17:09:04 +0100

    add release assistant

---
+++ scripts/ping_slack_about_package_release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+if [[ ${TRAVIS_TAG} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    curl -X POST -H "Content-type: application/json" \
+	 --data "{\"text\":\"💫 New *Rasa SDK* version ${TRAVIS_TAG} has been released! Changelog: https://github.com/RasaHQ/rasa-sdk/blob/${TRAVIS_TAG}/CHANGELOG.rst\"}" \
+	 "https://hooks.slack.com/services/T0GHWFTS8/BMTQQL47K/${SLACK_WEBHOOK_TOKEN}"
+fi
