b75700f61331e0f56129857aa1cb52856a502f7c
Ella Rohm-Ensing <erohmensing@gmail.com>
2019-11-27 17:18:18 +0100

    Apply suggestions from code review
    Co-Authored-By: Tom Bocklisch <tom@rasa.com>

--- scripts/ping_slack_about_package_release.sh
+++ scripts/ping_slack_about_package_release.sh
@@ -4,6 +4,6 @@ set -Eeuo pipefail
 
 if [[ ${TRAVIS_TAG} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     curl -X POST -H "Content-type: application/json" \
-	 --data "{\"text\":\"💫 New *Rasa SDK* version ${TRAVIS_TAG} has been released! Changelog: https://github.com/RasaHQ/rasa-sdk/blob/${TRAVIS_TAG}/CHANGELOG.rst\"}" \
+	 --data "{\"text\":\"😻 New *Rasa SDK* version ${TRAVIS_TAG} has been released! Changelog: https://github.com/RasaHQ/rasa-sdk/blob/${TRAVIS_TAG}/CHANGELOG.rst\"}" \
 	 "https://hooks.slack.com/services/T0GHWFTS8/BMTQQL47K/${SLACK_WEBHOOK_TOKEN}"
 fi
