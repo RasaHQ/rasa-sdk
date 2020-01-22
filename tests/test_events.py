d1bf1ffc69fddb9b4812916359ddcc45f774d2d1
Johannes E. M. Mosig <j.mosig@rasa.com>
2020-01-22 18:27:45 +0100

    Add tests for Reminder warnings

---
+++ tests/test_events.py
@@ -0,0 +1,26 @@
+import pytest
+
+import warnings
+from datetime import datetime
+
+import rasa_sdk.events as events
+
+
+def test_reminder_scheduled_correctly():
+    with warnings.catch_warnings(record=True) as warning:
+        events.ReminderScheduled("greet", datetime.now())
+        assert len(warning) == 0
+
+
+def test_reminder_scheduled_with_action():
+    with warnings.catch_warnings(record=True) as warning:
+        events.ReminderScheduled("action_greet", datetime.now())
+        assert len(warning) == 1
+        assert issubclass(warning[-1].category, DeprecationWarning)
+
+
+def test_reminder_scheduled_with_utter_action():
+    with warnings.catch_warnings(record=True) as warning:
+        events.ReminderScheduled("utter_greet", datetime.now())
+        assert len(warning) == 1
+        assert issubclass(warning[-1].category, DeprecationWarning)
