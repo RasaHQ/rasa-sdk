2c42289c29059456963682416717651f2ddb9dc0
Johannes E. M. Mosig <j.mosig@rasa.com>
2020-01-23 13:43:49 +0100

    Use `pytest.warns`

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,46 +1,34 @@
 import pytest
-
-import warnings
 from datetime import datetime
 
 import rasa_sdk.events as events
 
 
 def test_reminder_scheduled_correctly():
-    with warnings.catch_warnings(record=True) as warning:
+    with pytest.warns(None):
         events.ReminderScheduled("greet", datetime.now())
-        assert len(warning) == 0
 
 
 def test_reminder_scheduled_with_action():
-    with warnings.catch_warnings(record=True) as warning:
+    with pytest.warns(FutureWarning):
         events.ReminderScheduled("action_greet", datetime.now())
-        assert len(warning) == 1
-        assert issubclass(warning[-1].category, FutureWarning)
 
 
 def test_reminder_scheduled_with_utter_action():
-    with warnings.catch_warnings(record=True) as warning:
+    with pytest.warns(FutureWarning):
         events.ReminderScheduled("utter_greet", datetime.now())
-        assert len(warning) == 1
-        assert issubclass(warning[-1].category, FutureWarning)
 
 
 def test_reminder_cancelled_correctly():
-    with warnings.catch_warnings(record=True) as warning:
+    with pytest.warns(None):
         events.ReminderScheduled("greet", datetime.now())
-        assert len(warning) == 0
 
 
 def test_reminder_cancelled_with_action():
-    with warnings.catch_warnings(record=True) as warning:
+    with pytest.warns(FutureWarning):
         events.ReminderScheduled("action_greet", datetime.now())
-        assert len(warning) == 1
-        assert issubclass(warning[-1].category, FutureWarning)
 
 
 def test_reminder_cancelled_with_utter_action():
-    with warnings.catch_warnings(record=True) as warning:
+    with pytest.warns(FutureWarning):
         events.ReminderScheduled("utter_greet", datetime.now())
-        assert len(warning) == 1
-        assert issubclass(warning[-1].category, FutureWarning)
