758da923e05fc8190417f4cfdb87d2ec51250818
melindaloubser1 <melinda.loubser@gmail.com>
2020-08-31 01:41:53 +0200

    first docs structure

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,6 +1,13 @@
 module.exports = {
   someSidebar: {
-    'Rasa SDK': ['index'],
+    'Rasa SDK': [
+      'index',
+      'actions',
+      'tracker',
+      'events',
+      'dispatcher',
+      'running-action-server'
+    ],
     'Versioning': ['changelog'],
   },
 };
