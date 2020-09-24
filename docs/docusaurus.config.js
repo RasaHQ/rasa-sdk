c0b54cb01872167fb7c00c81265bd4c2b3730559
m-vdb <m.verger@rasa.com>
2020-09-24 12:13:58 +0200

    update docs config to the new theme

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,60 +1,115 @@
-const remarkSources = require('remark-sources');
+const path = require('path');
+const {
+  rehypePlugins: themeRehypePlugins,
+  remarkPlugins: themeRemarkPlugins,
+} = require('@rasahq/docusaurus-theme-tabula');
 const { remarkProgramOutput } = require("./plugins/program_output");
 
-let versions = [];
-try {
-  versions = require('./versions.json');
-} catch (ex) {
-  // Nothing to do here, in dev mode, only
-  // one version of the doc is available
-}
-let legacyVersions = [];
+const isDev = process.env.NODE_ENV === 'development';
+
+let existingVersions = [];
+try { existingVersions = require('./versions.json'); } catch (e) { console.info('no versions.json file found') }
+
+const BASE_URL = '/docs/action-server/';
+const SITE_URL = 'https://rasa.com';
+// NOTE: this allows switching between local dev instances of rasa/rasa-x
+const SWAP_URL = isDev ? 'http://localhost:3001' : SITE_URL;
+
+const versionLabels = {
+  current: 'Master/Unreleased'
+};
 
 module.exports = {
+  customFields: {
+    productLogo: '/img/logo-rasa-oss.png',
+    versionLabels,
+    redocPages: [
+      {
+        title: 'Rasa Action Server API',
+        specUrl: '/spec/action-server.yml',
+        slug: '/pages/action-server-api',
+      }
+    ]
+  },
   title: 'Rasa Action Server Documentation',
   tagline: 'Rasa Action Server Documentation',
-  url: 'https://rasa.com',
-  baseUrl: '/docs/action-server/',
-  favicon: 'img/favicon.ico',
+  url: SITE_URL,
+  baseUrl: BASE_URL,
+  favicon: '/img/favicon.ico',
   organizationName: 'RasaHQ',
   projectName: 'rasa-sdk',
   themeConfig: {
+    algolia: {
+      // this is configured via DocSearch here:
+      // https://github.com/algolia/docsearch-configs/blob/master/configs/rasa.json
+      apiKey: '25626fae796133dc1e734c6bcaaeac3c', // FIXME: replace with values from our own index
+      indexName: 'rasa',
+      inputSelector: '.search-bar',
+      searchParameters: {
+        'facetFilters': ["tags:rasa-action-server"]
+      }
+    },
     navbar: {
-      title: 'Action Server',
-      logo: {
-        alt: 'Rasa',
-        src: 'https://rasa.com/static/60e441f8eadef13bea0cc790c8cf188b/rasa-logo.svg',
-      },
+      hideOnScroll: false,
+      title: 'Rasa Action Server',
       items: [
         {
-          label: 'Docs',
-          to: '/', // "fake" link
+          label: 'Rasa Open Source',
+          href: `${SWAP_URL}/docs/rasa/`,
           position: 'left',
-          items: versions.length > 0 ? [
+        },
+        {
+          label: 'Rasa X',
+          position: 'left',
+          href: `${SWAP_URL}/docs/rasa-x/`,
+          target: '_self',
+        },
+        {
+          label: 'Rasa Action Server',
+          position: 'left',
+          to: path.join('/', BASE_URL),
+        },
+        {
+          href: 'https://github.com/rasahq/rasa',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+          position: 'right',
+        },
+        {
+          target: '_self',
+          href: 'https://blog.rasa.com/',
+          label: 'Blog',
+          position: 'right',
+        },
+        {
+          label: 'Community',
+          position: 'right',
+          items: [
             {
-              label: versions[0],
-              to: '/',
-              activeBaseRegex: versions[0],
+              target: '_self',
+              href: 'https://rasa.com/community/join/',
+              label: 'Community Hub',
             },
-            ...versions.slice(1).map((version) => ({
-              label: version,
-              to: `${version}/`,
-              activeBaseRegex: version,
-            })),
-            ...legacyVersions,
-          ] : [
             {
-              label: 'Latest',
-              to: '/',
-              activeBaseRegex: `/`,
+              target: '_self',
+              href: 'https://forum.rasa.com',
+              label: 'Forum',
             },
-            ...legacyVersions,
+            {
+              target: '_self',
+              href: 'https://rasa.com/community/contribute/',
+              label: 'How to Contribute',
+            },
+            {
+              target: '_self',
+              href: 'https://rasa.com/showcase/',
+              label: 'Community Showcase',
+            },
           ],
         },
       ],
     },
     footer: {
-      style: 'dark',
       copyright: `Copyright © ${new Date().getFullYear()} Rasa Technologies GmbH`,
     },
     gtm: {
@@ -62,27 +117,40 @@ module.exports = {
     },
   },
   themes: [
-    ['@docusaurus/theme-classic', {
-      customCss: require.resolve('./src/css/custom.css'),
-    }],
+    '@docusaurus/theme-search-algolia',
+    '@rasahq/docusaurus-theme-tabula',
   ],
   plugins: [
     ['@docusaurus/plugin-content-docs', {
       // https://v2.docusaurus.io/docs/next/docs-introduction/#docs-only-mode
       routeBasePath: '/',
-      // It is recommended to set document id as docs home page (`docs/` path).
-      homePageId: 'index',
       sidebarPath: require.resolve('./sidebars.js'),
       editUrl: 'https://github.com/rasahq/rasa-sdk/edit/master/docs/',
-      remarkPlugins: [
-        remarkSources,
-        remarkProgramOutput
+      showLastUpdateTime: true,
+      showLastUpdateAuthor: true,
+      rehypePlugins: [
+        ...themeRehypePlugins,
       ],
+      remarkPlugins: [
+        ...themeRemarkPlugins,
+        remarkProgramOutput,
+      ],
+      lastVersion: existingVersions[0] || 'current', // aligns / to last versioned folder in production
+      versions: {
+        current: {
+          label: versionLabels['current'],
+          path: existingVersions.length < 1 ? '' : 'next',
+        },
+      },
     }],
-    ['@docusaurus/plugin-sitemap', {
-      cacheTime: 600 * 1000, // 600 sec - cache purge period
-      changefreq: 'weekly',
-      priority: 0.5,
-    }],
-  ],
+    ['@docusaurus/plugin-content-pages', {}],
+    ['@docusaurus/plugin-sitemap',
+      {
+        cacheTime: 600 * 1000, // 600 sec - cache purge period
+        changefreq: 'weekly',
+        priority: 0.5,
+      }],
+    isDev && ['@docusaurus/plugin-debug', {}],
+    [path.resolve(__dirname, './plugins/google-tagmanager'), {}],
+  ].filter(Boolean),
 };
