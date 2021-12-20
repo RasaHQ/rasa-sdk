4e007e2d91e4b07a8960adc29de6a3580cf78b3e
Lu Nelson <lunelson@gmail.com>
2021-12-20 16:27:14 +0100

    WIP align config; prettyer

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,163 +1,18 @@
-const path = require('path');
-const {
-  rehypePlugins: themeRehypePlugins,
-  remarkPlugins: themeRemarkPlugins,
-} = require('@rasahq/docusaurus-theme-tabula');
-const { remarkProgramOutput } = require('./plugins/program_output');
+// @ts-check
 
-const isDev = process.env.NODE_ENV === 'development';
+const tabula = require('@rasahq/docusaurus-tabula');
 
-let existingVersions = [];
-try {
-  existingVersions = require('./versions.json');
-} catch (e) {
-  console.info('no versions.json file found');
-}
-
-const BASE_URL = '/docs/action-server/';
-const SITE_URL = 'https://rasa.com';
-// NOTE: this allows switching between local dev instances of rasa/rasa and rasa/rasa-x
-const RASA_OPEN_SOURCE_SWAP_URL = isDev ? 'http://localhost:3000' : SITE_URL;
-const RASA_X_SWAP_URL = isDev ? 'http://localhost:3001' : SITE_URL;
-
-const versionLabels = {
-  current: 'Main/Unreleased',
-};
-
-module.exports = {
-  customFields: {
-    productLogo: '/img/logo-rasa-oss.png',
-    versionLabels,
-    redocPages: [
-      {
-        title: 'Rasa Action Server API',
-        specUrl: '/spec/action-server.yml',
-        slug: '/pages/action-server-api',
-      },
-    ],
-  },
-  title: 'Rasa Action Server Documentation',
-  tagline: 'Rasa Action Server Documentation',
-  url: SITE_URL,
-  baseUrl: BASE_URL,
-  favicon: '/img/favicon.ico',
-  organizationName: 'RasaHQ',
-  projectName: 'rasa-sdk',
-  themeConfig: {
-    algolia: {
-      // this is configured via DocSearch here:
-      // https://github.com/algolia/docsearch-configs/blob/master/configs/rasa.json
-      apiKey: '1f9e0efb89e98543f6613a60f847b176',
-      indexName: 'rasa',
-      inputSelector: '.search-bar',
-      searchParameters: {
-        facetFilters: ['tags:rasa-action-server'],
-      },
+module.exports = tabula.use({
+  title: 'Rasa Action Server',
+  tagline: 'Rasa Action Server',
+  productLogo: '/img/logo-rasa-oss.png',
+  productKey: 'rasa-sdk',
+  staticDirectories: ['static'],
+  openApiSpecs: [
+    {
+      title: 'Rasa Action Server API',
+      specUrl: '/spec/action-server.yml',
+      slug: '/pages/action-server-api',
     },
-    navbar: {
-      hideOnScroll: false,
-      title: 'Rasa Action Server',
-      items: [
-        {
-          label: 'Rasa Open Source',
-          href: `${RASA_OPEN_SOURCE_SWAP_URL}/docs/rasa/`,
-          position: 'left',
-        },
-        {
-          label: 'Rasa X',
-          position: 'left',
-          href: `${RASA_X_SWAP_URL}/docs/rasa-x/`,
-          target: '_self',
-        },
-        {
-          label: 'Rasa Action Server',
-          position: 'left',
-          to: path.join('/', BASE_URL),
-        },
-        {
-          href: 'https://github.com/rasahq/rasa',
-          className: 'header-github-link',
-          'aria-label': 'GitHub repository',
-          position: 'right',
-        },
-        {
-          target: '_self',
-          href: 'https://blog.rasa.com/',
-          label: 'Blog',
-          position: 'right',
-        },
-        {
-          label: 'Community',
-          position: 'right',
-          items: [
-            {
-              target: '_self',
-              href: 'https://rasa.com/community/join/',
-              label: 'Community Hub',
-            },
-            {
-              target: '_self',
-              href: 'https://forum.rasa.com',
-              label: 'Forum',
-            },
-            {
-              target: '_self',
-              href: 'https://rasa.com/community/contribute/',
-              label: 'How to Contribute',
-            },
-            {
-              target: '_self',
-              href: 'https://rasa.com/showcase/',
-              label: 'Community Showcase',
-            },
-          ],
-        },
-      ],
-    },
-    footer: {
-      copyright: `Copyright © ${new Date().getFullYear()} Rasa Technologies GmbH`,
-    },
-    gtm: {
-      containerID: 'GTM-MMHSZCS',
-    },
-  },
-  themes: ['@docusaurus/theme-search-algolia', '@rasahq/docusaurus-theme-tabula'],
-  plugins: [
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        // https://v2.docusaurus.io/docs/next/docs-introduction/#docs-only-mode
-        routeBasePath: '/',
-        sidebarPath: require.resolve('./sidebars.js'),
-        editUrl: 'https://github.com/rasahq/rasa-sdk/edit/main/docs/',
-        showLastUpdateTime: true,
-        showLastUpdateAuthor: true,
-        rehypePlugins: [
-          // ...themeRehypePlugins,
-        ],
-        remarkPlugins: [
-          // ...themeRemarkPlugins,
-          remarkProgramOutput,
-        ],
-        lastVersion: existingVersions[0] || 'current', // aligns / to last versioned folder in production
-        versions: {
-          current: {
-            label: versionLabels['current'],
-            path: existingVersions.length < 1 ? '' : 'next',
-          },
-        },
-      },
-    ],
-    ['@docusaurus/plugin-content-pages', {}],
-    [
-      '@docusaurus/plugin-sitemap',
-      {
-        cacheTime: 600 * 1000, // 600 sec - cache purge period
-        changefreq: 'weekly',
-        priority: 0.5,
-      },
-    ],
-    isDev && ['@docusaurus/plugin-debug', {}],
-    [path.resolve(__dirname, './plugins/google-tagmanager'), {}],
-  ].filter(Boolean),
-};
+  ],
+});
--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,34 +1,28 @@
 module.exports = {
   someSidebar: {
-    'Intro': ['index'],
-    'All Action Servers': [
-      'actions',
-      'events'
-    ],
+    Intro: ['index'],
+    'All Action Servers': ['actions', 'events'],
     'Rasa SDK': [
-          'running-action-server',
+      'running-action-server',
+      {
+        type: 'category',
+        label: 'Writing Custom Actions',
+        collapsed: true,
+        items: [
+          'sdk-actions',
+          'sdk-tracker',
+          'sdk-dispatcher',
+          'sdk-events',
           {
-          type: 'category',
-          label: 'Writing Custom Actions',
-          collapsed: true,
-          items: [
-              'sdk-actions',
-              'sdk-tracker',
-              'sdk-dispatcher',
-            'sdk-events',
-            {
-              type: 'category',
-              label: 'Special Action Types',
-              collapsed: true,
-              items: [
-                'knowledge-bases',
-                  'validation-action',
-              ]
-              }
-          ],
+            type: 'category',
+            label: 'Special Action Types',
+            collapsed: true,
+            items: ['knowledge-bases', 'validation-action'],
           },
-          'deploy-action-server',
-          'rasa-sdk-changelog',
+        ],
+      },
+      'deploy-action-server',
+      'rasa-sdk-changelog',
     ],
   },
 };
