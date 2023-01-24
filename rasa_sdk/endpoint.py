8029bbc9a9cb7969e0b792814f766d8f5a20d47a
raoul.vonmetzen@telekom.de <raoul.vonmetzen@telekom.de>
2023-01-24 12:32:15 +0100

    first draft

--- rasa_sdk/endpoint.py
+++ rasa_sdk/endpoint.py
@@ -4,23 +4,258 @@ import os
 import types
 import zlib
 import json
-from typing import List, Text, Union, Optional, Any
+from typing import List, Text, Union, Optional, Any, Dict
+import ssl
 from ssl import SSLContext
 
 from sanic import Sanic, response
 from sanic.response import HTTPResponse
 from sanic.request import Request
 from sanic_cors import CORS
+import aiohttp 
+from aiohttp.client_exceptions import ContentTypeError
 
 from rasa_sdk import utils
 from rasa_sdk.cli.arguments import add_endpoint_arguments
 from rasa_sdk.constants import DEFAULT_SERVER_PORT
 from rasa_sdk.executor import ActionExecutor
 from rasa_sdk.interfaces import ActionExecutionRejection, ActionNotFoundException
+from rasa_sdk.exceptions import FileNotFoundException
+
+from urllib import parse
 
 logger = logging.getLogger(__name__)
 
+######### BLOCK INSERTION
 
+DOMAIN_ENDPOINT: str = os.environ("DOMAIN_ENDPOINT", "") # DOMAIN_ENDPOINT=http://rasa.server/domain:5055?token=TOKEN
+DEFAULT_REQUEST_TIMEOUT: int = 7 # give 7 seconds respone time for a domain API answer
+
+# copied from rasa.utils.endpoints
+class EndpointConfig:
+    """Configuration for an external HTTP endpoint."""
+
+    def __init__(
+        self,
+        url: Optional[Text] = None,
+        params: Optional[Dict[Text, Any]] = None,
+        headers: Optional[Dict[Text, Any]] = None,
+        basic_auth: Optional[Dict[Text, Text]] = None,
+        token: Optional[Text] = None,
+        token_name: Text = "token",
+        cafile: Optional[Text] = None,
+        **kwargs: Any,
+    ) -> None:
+        """Creates an `EndpointConfig` instance."""
+        self.url = url
+        self.params = params or {}
+        self.headers = headers or {}
+        self.basic_auth = basic_auth or {}
+        self.token = token
+        self.token_name = token_name
+        self.type = kwargs.pop("store_type", kwargs.pop("type", None))
+        self.cafile = cafile
+        self.kwargs = kwargs
+
+    def session(self) -> aiohttp.ClientSession:
+        """Creates and returns a configured aiohttp client session."""
+        # create authentication parameters
+        if self.basic_auth:
+            auth = aiohttp.BasicAuth(
+                self.basic_auth["username"], self.basic_auth["password"]
+            )
+        else:
+            auth = None
+
+        return aiohttp.ClientSession(
+            headers=self.headers,
+            auth=auth,
+            timeout=aiohttp.ClientTimeout(total=DEFAULT_REQUEST_TIMEOUT),
+        )
+
+    def combine_parameters(
+        self, kwargs: Optional[Dict[Text, Any]] = None
+    ) -> Dict[Text, Any]:
+        # construct GET parameters
+        params = self.params.copy()
+
+        # set the authentication token if present
+        if self.token:
+            params[self.token_name] = self.token
+
+        if kwargs and "params" in kwargs:
+            params.update(kwargs["params"])
+            del kwargs["params"]
+        return params
+
+    async def request(
+        self,
+        method: Text = "post",
+        subpath: Optional[Text] = None,
+        content_type: Optional[Text] = "application/json",
+        **kwargs: Any,
+    ) -> Optional[Any]:
+        """Send a HTTP request to the endpoint. Return json response, if available.
+
+        All additional arguments will get passed through
+        to aiohttp's `session.request`."""
+
+        # create the appropriate headers
+        headers = {}
+        if content_type:
+            headers["Content-Type"] = content_type
+
+        if "headers" in kwargs:
+            headers.update(kwargs["headers"])
+            del kwargs["headers"]
+
+        url = concat_url(self.url, subpath)
+
+        sslcontext = None
+        if self.cafile:
+            try:
+                sslcontext = ssl.create_default_context(cafile=self.cafile)
+            except FileNotFoundError as e:
+                raise FileNotFoundException(
+                    f"Failed to find certificate file, "
+                    f"'{os.path.abspath(self.cafile)}' does not exist."
+                ) from e
+
+        async with self.session() as session:
+            async with session.request(
+                method,
+                url,
+                headers=headers,
+                params=self.combine_parameters(kwargs),
+                ssl=sslcontext,
+                **kwargs,
+            ) as response:
+                if response.status >= 400:
+                    raise ClientResponseError(
+                        response.status, response.reason, await response.content.read()
+                    )
+                try:
+                    return await response.json()
+                except ContentTypeError:
+                    return None
+
+    @classmethod
+    def from_dict(cls, data: Dict[Text, Any]) -> "EndpointConfig":
+        return EndpointConfig(**data)
+
+    def copy(self) -> "EndpointConfig":
+        return EndpointConfig(
+            self.url,
+            self.params,
+            self.headers,
+            self.basic_auth,
+            self.token,
+            self.token_name,
+            **self.kwargs,
+        )
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(self, type(other)):
+            return (
+                other.url == self.url
+                and other.params == self.params
+                and other.headers == self.headers
+                and other.basic_auth == self.basic_auth
+                and other.token == self.token
+                and other.token_name == self.token_name
+            )
+        else:
+            return False
+
+    def __ne__(self, other: Any) -> bool:
+        return not self.__eq__(other)
+
+# copied from rasa.utils.endpoints
+class ClientResponseError(aiohttp.ClientError):
+    def __init__(self, status: int, message: Text, text: Text) -> None:
+        self.status = status
+        self.message = message
+        self.text = text
+        super().__init__(f"{status}, {message}, body='{text}'")
+
+# copied from rasa.utils.endpoints
+def concat_url(base: Text, subpath: Optional[Text]) -> Text:
+    """Append a subpath to a base url.
+
+    Strips leading slashes from the subpath if necessary. This behaves
+    differently than `urlparse.urljoin` and will not treat the subpath
+    as a base url if it starts with `/` but will always append it to the
+    `base`.
+
+    Args:
+        base: Base URL.
+        subpath: Optional path to append to the base URL.
+
+    Returns:
+        Concatenated URL with base and subpath.
+    """
+    if not subpath:
+        if base.endswith("/"):
+            logger.debug(
+                f"The URL '{base}' has a trailing slash. Please make sure the "
+                f"target server supports trailing slashes for this endpoint."
+            )
+        return base
+
+    url = base
+    if not base.endswith("/"):
+        url += "/"
+    if subpath.startswith("/"):
+        subpath = subpath[1:]
+    return url + subpath
+
+local_domain = dict()
+domain_endpoint = None
+
+if DOMAIN_ENDPOINT: 
+    # TODO that could be a file URL to be loaded, like in rasa.utils.endpoints.read_endpoint_config()
+    if isinstance(DOMAIN_ENDPOINT, str) and DOMAIN_ENDPOINT.startswith("http"):
+        parts = parse.urlparse(DOMAIN_ENDPOINT)
+        query = parts.query
+        query_params = {k:(v[0] if len(v)==1 else v) for k,v in parse.parse_qs(query)}
+        parts.query = None
+        parts.fragment = None
+        url = parse.urlunparse(parts)
+        token = {}
+        if t:=query_params.get("token"):
+            # special case for token
+            token["token_name"]="token"
+            token["token"]=t
+            del query_params["token"]
+        logger.debug(f"{url=} {query_params=} {token=}")
+        domain_endpoint = EndpointConfig(url=url,  params=query_params, **token)
+
+async def get_domain()->dict:
+    if local_domain:
+        # use cached
+        return local_domain
+    if domain_endpoint:
+        # create a request towards the endpoint which can deliver a domain JSON
+        try:
+            logger.debug(f"Accessing domain endpoint at {DOMAIN_ENDPOINT}")
+            response: Any = await domain_endpoint.request(
+                method="get", timeout=DEFAULT_REQUEST_TIMEOUT
+            )            
+            if "intents" in response:
+                # very simple check if it is a domain objects
+                local_domain = response # put in cache
+                return local_domain
+        except ClientResponseError as ex:
+                logger.error(f"Retrieval of domain was unsuccessful, {ex.message=} {ex.status=} {ex.text=}", exc_info=ex)
+                return 
+        except Exception as e:
+                logger.error(f"Retrieval of domain was unsuccessful, {e.args=}", exc_info=ex)
+                return             
+
+
+
+
+ ########## END BLOCK INSERTION
 def configure_cors(
     app: Sanic, cors_origins: Union[Text, List[Text], None] = ""
 ) -> None:
@@ -99,6 +334,8 @@ def create_app(
             action_call = json.loads(decompressed_data)
         else:
             action_call = request.json
+            
+
         if action_call is None:
             body = {"error": "Invalid body request"}
             return response.json(body, status=400)
@@ -107,6 +344,12 @@ def create_app(
 
         if auto_reload:
             executor.reload()
+
+        if not action_call.get("domain"):
+            # no domain send by Rasa OSS, so use chached one or try to acquire one from endpoint
+            domain = await get_domain()
+            if domain:
+                action_call["domain"] = domain
 
         try:
             result = await executor.run(action_call)
