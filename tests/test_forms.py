cba91fadfc250ecb4ca84b159f358d1898f9fbf4
d61h6k4 <ddbihbka@gmail.com>
2023-03-13 14:18:25 +0100

    Add support of validation of slots with . in name

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -12,7 +12,6 @@ from rasa_sdk.forms import (
 )
 from rasa_sdk.slots import SlotMapping
 
-
 DEFAULT_DOMAIN = {
     "slots": {
         "some_other_slot": {
@@ -759,9 +758,9 @@ async def test_extract_slot_only():
         (["my_slot", "other_slot"], {}, [SlotSet(REQUESTED_SLOT, "other_slot")]),
         # Extract method for slot which is also mapped in domain
         (
-            ["my_slot"],
+            ["my_slot", "other_slot"],
             {"forms": {"some_form": {"required_slots": ["my_slot"]}}},
-            [],
+            [SlotSet(REQUESTED_SLOT, "other_slot")],
         ),
     ],
 )
@@ -874,3 +873,50 @@ async def test_ask_for_next_slot(
     dispatcher = CollectingDispatcher()
     events = await form.run(dispatcher=dispatcher, tracker=tracker, domain=domain)
     assert events == expected_return_events
+
+
+async def test_form_validation_space_slot():
+    form_name = "some_form"
+
+    class TestFormValidationSpaceSlotAction(FormValidationAction):
+        def name(self) -> Text:
+            return form_name
+
+        def validate_space_slot(
+            self,
+            slot_value: Any,
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: "DomainDict",
+        ) -> Dict[Text, Any]:
+            if slot_value == "correct_value":
+                return {
+                    "space.slot": "validated_value",
+                }
+            return {
+                "space.slot": None,
+            }
+
+    form = TestFormValidationSpaceSlotAction()
+
+    # tracker with active form
+    tracker = Tracker(
+        "default",
+        {},
+        {},
+        [SlotSet("space.slot", "correct_value")],
+        False,
+        None,
+        {"name": form_name, "is_interrupted": False, "rejected": False},
+        "action_listen",
+    )
+
+    dispatcher = CollectingDispatcher()
+    events = await form.run(
+        dispatcher=dispatcher,
+        tracker=tracker,
+        domain={"forms": {form_name: {"required_slots": ["space.slot"]}}},
+    )
+    assert events == [
+        SlotSet("space.slot", "validated_value"),
+    ]
