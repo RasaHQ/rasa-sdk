48e9f74ea2794e19f73ffa299c5a0abf0446cb04
Milos Pavlic <m.pavlic@rasa.com>
2025-08-19 16:16:19 +0200

    Update import in the generated grpc code

--- rasa_sdk/grpc_py/action_webhook_pb2_grpc.py
+++ rasa_sdk/grpc_py/action_webhook_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import action_webhook_pb2 as action__webhook__pb2
+from rasa_sdk.grpc_py import action_webhook_pb2 as action__webhook__pb2
 
 GRPC_GENERATED_VERSION = '1.67.1'
 GRPC_VERSION = grpc.__version__
