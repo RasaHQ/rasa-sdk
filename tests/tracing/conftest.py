4a93a61f6deb44510ceca0f93556c015aa19e3b2
Tanja Bunk <tabergma@gmail.com>
2025-09-10 15:01:37 +0200

    fix imports

--- tests/tracing/conftest.py
+++ tests/tracing/conftest.py
@@ -8,8 +8,6 @@ import grpc
 import opentelemetry.proto.collector.trace.v1.trace_service_pb2_grpc as trace_service
 import pytest
 from google.protobuf.internal.containers import RepeatedCompositeFieldContainer
-from opentelemetry.exporter.jaeger.thrift.gen.agent.Agent import emitBatch_args
-from opentelemetry.exporter.jaeger.thrift.gen.jaeger.ttypes import Batch
 from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import (
     ExportTraceServiceRequest,
     ExportTraceServiceResponse,
@@ -68,14 +66,3 @@ def grpc_server(
     server.start()
     yield server
     server.stop(None)
-
-
-def deserialize_jaeger_batch(data: bytearray) -> Batch:
-    trans = TMemoryBuffer(data)
-    prot = TCompactProtocol(trans)
-    prot.readMessageBegin()
-    emitBatch = emitBatch_args()  # type: ignore
-    emitBatch.read(prot)  # type: ignore
-    prot.readMessageEnd()
-
-    return emitBatch.batch
