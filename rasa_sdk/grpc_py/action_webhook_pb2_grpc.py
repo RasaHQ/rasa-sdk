442e708ca4c94350d5d4dbf247b830e10bad2c23
Tanja Bunk <tabergma@gmail.com>
2025-09-12 09:51:24 +0200

    update grpc_py

--- rasa_sdk/grpc_py/action_webhook_pb2_grpc.py
+++ rasa_sdk/grpc_py/action_webhook_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from rasa_sdk.grpc_py import action_webhook_pb2 as rasa__sdk_dot_grpc__py_dot_action__webhook__pb2
 
-GRPC_GENERATED_VERSION = '1.71.2'
+GRPC_GENERATED_VERSION = '1.66.2'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
