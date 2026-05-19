a60c975652b34158585802bb9d9a8c1070948926
Anca Lita <27920906+ancalita@users.noreply.github.com>
2026-05-19 17:51:13 +0100

    remove unused import

--- rasa_sdk/grpc_py/action_webhook_pb2.pyi
+++ rasa_sdk/grpc_py/action_webhook_pb2.pyi
@@ -1,5 +1,4 @@
 from google.protobuf import struct_pb2 as _struct_pb2
-from google.protobuf import empty_pb2 as _empty_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
